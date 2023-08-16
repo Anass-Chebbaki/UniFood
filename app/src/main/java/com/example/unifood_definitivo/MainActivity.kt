08874534e1976be01faf7bc5edab144bb26b38f5
Vincenzo Guadagnini <vinceguadagnini@gmail.com>
2023-08-16 19:20:56 +0200

    jetifiers

--- app/src/main/java/com/example/unifood_definitivo/MainActivity.kt
+++ app/src/main/java/com/example/unifood_definitivo/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
             finish()
-            //prova prova ciao ciao
+            //dioslabratoassassino
         }
 
 
