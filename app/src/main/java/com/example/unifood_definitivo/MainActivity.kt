0117fe7cffc57de38e006deb5690ceb25863c401
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2023-08-16 17:20:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/unifood_definitivo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.loginsignupauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import com.example.unifood_definitivo.R
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val isLoggedIn = firebaseAuth.currentUser != null
+
+        if (!isLoggedIn) {
+            // Utente non registrato o non ha effettuato l'accesso
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+            finish()
+        }
+
+
+    }
+}
