fd77f2f72199a432e6969926276d1bc43bf0d686
Anass-Chebbaki <S1099007@studenti.univpm.it>
2023-08-23 11:31:56 +0200

    NUOVO PUNTO DI PARTENZAAAAAAA!

--- app/src/main/java/com/example/unifood_definitivo/MainActivity.kt
+++ app/src/main/java/com/example/unifood_definitivo/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.widget.TextView
-import com.example.unifood_definitivo.Model.CiboData
+
 import com.example.unifood_definitivo.R
 import com.google.firebase.auth.FirebaseAuth
 
