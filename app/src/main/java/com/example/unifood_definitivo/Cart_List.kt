e50985eda0cbe2551715b50d94cfeb04c2c108ff
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2023-08-26 16:05:51 +0200

    no è il momento

---
+++ app/src/main/java/com/example/unifood_definitivo/Cart_List.kt
@@ -0,0 +1,21 @@
+ package com.example.unifood_definitivo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.loginsignupauth.MainActivity
+
+
+import com.example.unifood_definitivo.Model.Prodotti
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+ class Cart_List : AppCompatActivity() {
+
+
+
+ }
--- app/src/main/java/com/example/unifood_definitivo/Show_details.kt
+++ app/src/main/java/com/example/unifood_definitivo/Show_details.kt
@@ -1,5 +1,6 @@
 package com.example.unifood_definitivo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -8,6 +9,9 @@ import com.example.unifood_definitivo.Model.Prodotti
 import com.squareup.picasso.Picasso
 
 class Show_details : AppCompatActivity() {
+    private lateinit var quantityTextView: TextView
+    private var quantity: Int = 1 // Initial quantity value
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_details)
@@ -17,12 +21,12 @@ class Show_details : AppCompatActivity() {
             // Populate your layout with the details of the selected product
             val productNameTextView = findViewById<TextView>(R.id.titleTxt)
             val productPriceTextView = findViewById<TextView>(R.id.priceTxt)
-            val productDescriptionTextView=findViewById<TextView>(R.id.ingredientsTxt)
+            val productDescriptionTextView = findViewById<TextView>(R.id.ingredientsTxt)
             // ... (other views you want to populate)
 
             productNameTextView.text = product.nome_prodotto
             productPriceTextView.text = product.prezzo.toString()
-            productDescriptionTextView.text=product.ingredienti.toString()
+            productDescriptionTextView.text = product.ingredienti.toString()
 
 
             // Load and display the image using Picasso or any other image loading library
@@ -30,9 +34,46 @@ class Show_details : AppCompatActivity() {
             product.imgUri2?.let {
                 Picasso.get().load(it).into(productImageView)
             }
+            val addToCartButton = findViewById<TextView>(R.id.addToCardBtn)
+            addToCartButton.setOnClickListener {
+                val intent = Intent(this, Cart_List::class.java)
+                intent.putExtra("product", product)
+                intent.putExtra("quantity", quantity)
+                startActivity(intent)
+            }
+            quantityTextView = findViewById(R.id.quantityTxt)
+            quantityTextView.text = quantity.toString()
+
+            val plusButton = findViewById<ImageView>(R.id.plusBtn)
+            val minusButton = findViewById<ImageView>(R.id.minusBtn)
+
+            plusButton.setOnClickListener {
+                increaseQuantity()
+            }
+
+            minusButton.setOnClickListener {
+                decreaseQuantity()
+            }
         } else {
             // Handle the case where the product data is missing or invalid
             // You might want to show an error message or return to the previous screen
         }
+
+
+    }
+    private fun increaseQuantity() {
+        quantity++
+        updateQuantity()
+    }
+
+    private fun decreaseQuantity() {
+        if (quantity > 1) {
+            quantity--
+            updateQuantity()
+        }
+    }
+
+    private fun updateQuantity() {
+        quantityTextView.text = quantity.toString()
     }
 }
